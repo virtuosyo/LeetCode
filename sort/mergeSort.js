d5aded4b83cc04cd710e572195802cc325d2373e
yang_going <2210744381@qq.com>
2020-05-25 19:41:26 +0800

    归并排序

---
+++ sort/mergeSort.js
@@ -0,0 +1,41 @@
+const arr = [1, 4, 5, 3, 2]
+const merge = (left, right) => {
+    let i = 0
+    let j = 0
+    const result = []
+    while (i < left.length && j < right.length) {
+        result.push(left[i] < right[j] ? left[i++] : right[j++])
+    }
+    return result.concat(i < left.length ? left.slice(i) : right.slice(j))
+}
+const mergeSort = array => {
+    if (array.length > 1) {
+        const middle = Math.floor(array.length / 2)
+        const left = mergeSort(array.slice(0, middle))
+        const right = mergeSort(array.slice(middle, array.length))
+        array = merge(left, right)
+    }
+    return array
+}
+
+
+
+const mergeSort = arr => {
+    if (arr.length > 1) {
+        let middle = Math.floor(arr.length / 2)
+        let left = mergeSort(arr.slice(0, middle))
+        let right = mergeSort(arr.slice(middle, arr.length))
+        arr = merge(left, right)
+    }
+    return arr
+}
+const merge = (left, right) => {
+    let [i, j] = [0, 0]
+    let result = []
+    while (i < left.length && j < right.length) {
+        result.push(left[i] < right[j] ? left[i++] : right[j++])
+    }
+    return result.concat(i < left.length ? left[i] : right[j])
+}
+
+console.log(mergeSort(arr));
