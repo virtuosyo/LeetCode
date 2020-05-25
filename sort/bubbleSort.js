1aadf143522a0ce3161775e49fcd8424483d1d71
yang_going <2210744381@qq.com>
2020-05-25 19:41:01 +0800

    冒泡

---
+++ sort/bubbleSort.js
@@ -0,0 +1,24 @@
+const arr = [1, 4, 5, 3, 2]
+const bubbleSort = array => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort(arr));
+
+const bubbleSort = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
