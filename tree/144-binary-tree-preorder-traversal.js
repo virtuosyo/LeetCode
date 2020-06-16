24321c1075d7ef6fcc561ede21bcae1600c71e3d
yang_going <2210744381@qq.com>
2020-06-16 17:24:19 +0800

    144-二叉树的先序遍历(递归&迭代)

--- tree/144-binary-tree-preorder-traversal.js
+++ tree/144-binary-tree-preorder-traversal.js
@@ -21,3 +21,18 @@ var preorderTraversal = function (root) {
     preOrderTraverseNode(root)
     return arr
 };
+
+var preorderTraversal = function (root) {
+    let res = []
+    let stack = [root]
+    let p = root
+    while (stack.length) {
+        const n = stack.pop()
+        if (n) {
+            res.push(n.val)
+            if (n.right) stack.push(n.right)
+            if (n.left) stack.push(n.left)
+        }
+    }
+    return res
+};
