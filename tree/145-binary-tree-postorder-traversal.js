74f016a278a7f13ab13828bcf4ea3baf82b44310
yang_going <2210744381@qq.com>
2020-06-16 17:24:30 +0800

    198-二叉树的后序遍历(递归&迭代)

--- tree/145-binary-tree-postorder-traversal.js
+++ tree/145-binary-tree-postorder-traversal.js
@@ -21,3 +21,23 @@ var postorderTraversal = function (root) {
     postOrderTraverseNode(root)
     return arr
 };
+
+var postorderTraversal = function (root) {
+    let res = []
+    let stack = [root]
+    let outputStack = []
+    let p = root
+    while (stack.length) {
+        const n = stack.pop()
+        if (n) {
+            outputStack.push(n)
+            if (n.left) stack.push(n.left)
+            if (n.right) stack.push(n.right)
+        }
+    }
+    while (outputStack.length) {
+        const n = outputStack.pop()
+        res.push(n.val)
+    }
+    return res
+};
