353f1a21d8068c30d784b60d930be7ce66a45ff0
yang_going <2210744381@qq.com>
2020-06-21 13:23:07 +0800

    123-买卖股票的最佳时机

---
+++ dynamic-programming/123-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let min = Infinity
+    let max = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i]
+        } else {
+            max = Math.max(max, prices[i] - min)
+        }
+    }
+    return max
+}
