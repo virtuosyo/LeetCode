89d533bc7de12d21e50a81a9e89594473bde38e5
yang_going <2210744381@qq.com>
2020-06-26 22:35:44 +0800

    110-平衡二叉树

---
+++ tree/110-balanced-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    return balanced(root) !== -1
+};
+var balanced = function (node) {
+    if (!node) return 0
+    const left = balanced(node.left)
+    const right = balanced(node.right)
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1
+    }
+    return Math.max(left, right) + 1
+}
