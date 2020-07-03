107a38277235afc9ad4787b3ec31110af4440242
yang_going <2210744381@qq.com>
2020-07-03 16:48:53 +0800

    冒泡排序

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -11,14 +11,3 @@ const bubbleSort = array => {
 }
 
 console.log(bubbleSort(arr));
-
-const bubbleSort = arr => {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-            }
-        }
-    }
-    return arr
-}
