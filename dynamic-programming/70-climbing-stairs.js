34f303df213ca5a03ebd9f2f06c525873ed84732
Yanggoing <yanggoing@qq.com>
2020-07-14 23:45:15 +0800

    70-爬楼梯

--- dynamic-programming/70-climbing-stairs.js
+++ dynamic-programming/70-climbing-stairs.js
@@ -21,6 +21,7 @@ var climbStairs = function (n) {
     let dp0 = 1
     let dp1 = 1
     for (let i = 2; i <= n; i++) {
+        // 临时变量
         const t = dp0
         dp0 = dp1
         dp1 = dp1 + t
