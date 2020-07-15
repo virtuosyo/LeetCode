58228d16599a64296233231f0d1d5d16f620bfd1
Yanggoing <yanggoing@qq.com>
2020-07-15 23:25:10 +0800

    198-打家劫舍

--- dynamic-programming/198-house-robber.js
+++ dynamic-programming/198-house-robber.js
@@ -1,17 +1,28 @@
-let arr = [1, 2, 3, 1]
-
-function rec_opt(arr, i) {
-    if (i === 0) {
-        return arr[0]
-    } else if (i === 1) {
-        return Math.max(arr[0], arr[1])
-    } else {
-        let A = rec_opt(arr, i - 2) + arr[i]
-        let B = rec_opt(i - 1)
-        return Math.max(A, B)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    if (nums.length === 0) {
+        return 0
     }
-}
-console.log(rec_opt(arr,3));
-// var rob = function (nums) {
+    const dp = [0, nums[0]]
+    for (let i = 2; i <= nums.length; i++) {
+        dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1])
+    }
+    return dp[dp.length - 1]
+};
 
-// };
+var rob = function (nums) {
+    if (nums.length === 0) {
+        return 0
+    }
+    let dp0 = 0
+    let dp1 = nums[0]
+    for (let i = 2; i <= nums.length; i++) {
+        const dp2 = Math.max(dp0 + nums[i - 1], dp1)
+        dp0 = dp1
+        dp1 = dp2
+    }
+    return dp1
+};
